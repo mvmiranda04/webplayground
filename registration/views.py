b67e49021c20cbc08c212d4985dc32336683f39b
Marta Miranda <mvmiranda@catamarca.gov.ar>
2021-11-05 11:52:21 -0300

    Modificaciones 5 de noviembre ultima hora

--- registration/views.py
+++ registration/views.py
@@ -2,10 +2,14 @@ from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 from django import forms
+from .forms import UserCreationFormWithEmail
 
 
 class SignUpView(CreateView):
-    form_class = UserCreationForm
+    # Ya no usamo UserCreationForm para usar el que agrega el email
+    # form_class = UserCreationForm
+    form_class = UserCreationFormWithEmail
+
     # Lo comenta para concatenarle un variale que se registro exitosamente
     # success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
@@ -16,11 +20,13 @@ class SignUpView(CreateView):
     def get_form(self, form_class=None):
         form = super(SignUpView, self).get_form()
         # Mofificar en tiempo real
-        form.fields['username'].widget = forms.TextInput(attrs={'class': 'form-control mb-2', 'placeholder':
-            'Nombre de usuario'})
-        form.fields['password1'].widget = forms.PasswordInput(attrs={'class': 'form-control mb-2', 'placeholder':
-            'Contraseña'})
-        form.fields['password2'].widget = forms.PasswordInput(attrs={'class': 'form-control mb-2', 'placeholder':
-            'Repite Contraseña'})
+        form.fields['username'].widget = forms.TextInput(attrs={'class': 'form-control mb-2', 'placeholder': 'Nombre '
+                                                                                                             'de '
+                                                                                                             'usuario'})
+        # Agregamos el campo email
+        form.fields['email'].widget = forms.EmailInput(attrs={'class': 'form-control mb-2', 'placeholder': 'Direccion '
+                                                                                                           'de email'})
+        form.fields['password1'].widget = forms.PasswordInput(attrs={'class': 'form-control mb-2', 'placeholder': 'Contraseña'})
+        form.fields['password2'].widget = forms.PasswordInput(attrs={'class': 'form-control mb-2', 'placeholder': 'Repite Contraseña'})
 
         return form
